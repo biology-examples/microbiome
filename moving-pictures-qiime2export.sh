526966fb274348efcd582179618f26678f3fc3a9
k8hertweck <k8hertweck@gmail.com>
2017-12-05 14:48:54 -0600

    updating directory name and version

--- moving-pictures-qiime2export.sh
+++ moving-pictures-qiime2export.sh
@@ -6,23 +6,23 @@
 # first run moving-pictures-qiime2.sh
 
 ## activate the environment
-source activate qiime2-2017.10
+source activate qiime2-2017.11
 # test installation
 #qiime --help
 
 ## export data and prepare for working in R
-mkdir qiime-exported
+mkdir qiime2-exported
 # copy mapping file to exported file
-cp qiime2-moving-pictures-tutorial/sample-metadata.tsv qiime-exported/
+cp qiime2-moving-pictures-tutorial/sample-metadata.tsv qiime2-exported/
 
 cd qiime2-moving-pictures-tutorial/
 
 # export data from qza
-qiime tools export taxonomy.qza --output-dir ../qiime-exported
-qiime tools export rooted-tree.qza --output-dir ../qiime-exported
-qiime tools export table.qza --output-dir ../qiime-exported
+qiime tools export taxonomy.qza --output-dir ../qiime2-exported
+qiime tools export rooted-tree.qza --output-dir ../qiime2-exported
+qiime tools export table.qza --output-dir ../qiime2-exported
 
-cd ../qiime-exported/
+cd ../qiime2-exported/
 
 # add taxonomy to biom table
 biom add-metadata -i feature-table.biom -o table.biom --observation-metadata-fp taxonomy.tsv --observation-header OTUID,taxonomy,confidence
